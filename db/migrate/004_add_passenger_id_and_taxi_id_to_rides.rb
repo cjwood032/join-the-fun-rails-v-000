0d4e37fb18017b6d5b8ab44002548d698e61edc3
Christopher Wood <cjwood032@gmail.com>
2018-08-06 18:51:49 +0000

    automatically backed up by learn

--- db/migrate/004_add_passenger_id_and_taxi_id_to_rides.rb
+++ db/migrate/004_add_passenger_id_and_taxi_id_to_rides.rb
@@ -1,4 +1,4 @@
-class AddPassengerIdAndTaxiIdToRides < ActiveRecord::Migration
+class Rides < ActiveRecord::Migration
   def change
     add_column :rides, :passenger_id, :integer
     add_column :rides, :taxi_id, :integer
