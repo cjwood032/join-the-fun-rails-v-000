5c0702dde0ee0052d8728201ebe389e9862b0c31
Christopher Wood <cjwood032@gmail.com>
2018-08-06 18:57:13 +0000

    automatically backed up by learn

--- db/migrate/20140212205242_create_rides.rb
+++ db/migrate/20140212205242_create_rides.rb
@@ -2,6 +2,8 @@ class CreateRides < ActiveRecord::Migration
   def change
     create_table :rides do |t|
       t.timestamps null: false
+      :passenger_id, :integer
+      :taxi_id, :integer
     end
   end
 end
